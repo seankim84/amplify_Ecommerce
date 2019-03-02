08d3ee7be2038ebe42317bf4caebcd4b778182b8
SeanKim <sean.kim84@gmail.com>
2019-03-02 18:47:37 +0700

    Basic Configure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import  { AmplifyTheme, withAuthenticator }  from 'aws-amplify-react';
 import "./App.css";
 
 class App extends React.Component {
@@ -9,4 +10,25 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const theme = {
+  ...AmplifyTheme,
+  navBar: {
+    ...AmplifyTheme.navBar,
+    backgroundColor: '#223973'
+  },
+  button: {
+    ...AmplifyTheme.button,
+    background: '#223973',
+    color: '#fffff'
+  },
+  sectionBody: {
+    ...AmplifyTheme.sectionBody,
+    padding: '5px'
+  },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    background: '#223973'
+  }
+}
+
+export default withAuthenticator(App, true, [], null, theme);
